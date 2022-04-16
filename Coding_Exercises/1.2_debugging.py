80268918bbca8d8960a8f906361eab91899218ff
Mian Azan <mianazan099@gmail.com>
2022-04-16 11:39:14 +0500

    day 1 completed

---
+++ Coding_Exercises/1.2_debugging.py
@@ -0,0 +1,11 @@
+# Fix This Code
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#   print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")
+
+# Fixed
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
