ab40fe72adf2c810653d7ae52b8bf279046b11cc
Mian Azan <mianazan099@gmail.com>
2022-04-28 21:49:32 +0500

    Day 10

---
+++ 10_Calculator/main.py
@@ -0,0 +1,40 @@
+from art import logo
+from os import system
+
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
+operations = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide,
+}
+while True:
+    print(logo)
+    num1 = float(input('What\'s the first number?: '))
+    print('+\n-\n*\n/')
+    continue_calc = 'y'
+    while continue_calc == 'y':
+        operation = input('Pick an operation: ')
+        num2 = float(input('What\'s the next number?: '))
+        calculation = operations[f'{operation}'](num1, num2)
+        print(f'{num1} {operation} {num2} = {calculation}')
+        continue_calc = input(
+            f'Type \'y\' to continue calculating with {calculation}, or type \'n\' to start a new calculation: ').lower()
+        num1 = calculation
+    system('cls' or 'clear')
