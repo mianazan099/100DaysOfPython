76b4488c0b3b8c0bda1792a2527ca3c55aca175a
Mian Azan <mianazan099@gmail.com>
2022-05-03 15:47:35 +0500

    Day 12 Completed

--- 12_Guess-The-Number/main.py
+++ 12_Guess-The-Number/main.py
@@ -1,9 +1,29 @@
-# Number Guessing Game Objectives:
-
-# Include an ASCII art logo.
-# Allow the player to submit a guess for a number between 1 and 100.
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer.
-# If they got the answer correct, show the actual answer to the player.
-# Track the number of turns remaining.
-# If they run out of turns, provide feedback to the player.
-# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+from art import logo
+from random import randint
+print(logo)
+print('Welcome to the Number Guessing Game!')
+print('I\'m thinking of a number between 1 and 100.')
+number = randint(1, 100)
+level = input('Choose a difficulty. Type \'easy\' or \'hard\': ').lower()
+if level == 'easy':
+    lives = 10
+else:
+    lives = 5
+game_over = False
+while not game_over:
+    print(f'You have {lives} attempts remaining to guess the number.')
+    guess = int(input('Make a guess: '))
+    if guess == number:
+        game_over = True
+        print(f'You got it! The answer was {number}.')
+    else:
+        if guess < number:
+            print('Too low.')
+        elif guess > number:
+            print('Too high.')
+        lives -= 1
+        if lives == 0:
+            game_over = True
+            print('You\'ve run out of guesses, you lose.')
+        else:
+            print('Guess again.')
