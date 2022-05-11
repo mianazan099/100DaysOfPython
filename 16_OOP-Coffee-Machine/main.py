991161c6fb8dd5fdec19241c03febf242c38c0e7
Mian Azan <mianazan099@gmail.com>
2022-05-11 11:51:58 +0500

    day 16

--- 16_OOP-Coffee-Machine/main.py
+++ 16_OOP-Coffee-Machine/main.py
@@ -1,6 +1,22 @@
-from turtle import Screen, Turtle
-jojo = Turtle()
-jojo.color('teal')
-jojo.shape('turtle')
-my_screen = Screen()
-my_screen.exitonclick()
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+# menuitem = MenuItem('coffee')
+# menuitem.name
+# menuitem.cost
+# menuitem.ingredients
+# menu = Menu()
+# menu.get_items()
+# menu.find_drink('')
+# coffeemaker = CoffeeMaker()
+# coffeemaker.report()
+# coffeemaker.is_resource_sufficient('')
+# coffeemaker.make_coffee('')
+# moneymachine = MoneyMachine()
+# moneymachine.report()
+# moneymachine.make_payment(12)
+accessKey = "y9IofoQ3_Bl0ygwIcr34gKOFmafd_M9K-GEofQzlAAY"
+secretKey = "HHXVec2ivYNKHyoK_FL2NNLRJ48khC2LO-mV2lwEOjw"
+URL = f"https://api.unsplash.com/photos/?client_id={accessKey}"
+print(URL)
---
+++ 16_OOP-Coffee-Machine/practice.py
@@ -0,0 +1,28 @@
+
+
+############ Practice ############
+# from turtle import Turtle
+# import turtle
+# timmy = Turtle()
+# timmy.color('coral', 'cyan')
+# timmy.begin_fill()
+# timmy.shape('turtle')
+# while True:
+#     timmy.forward(400)
+#     timmy.left(176)
+#     if abs(timmy.pos()) < 1:
+#         break
+# timmy.end_fill()
+# timmy.hideturtle()
+# turtle.done()
+
+# from prettytable import PrettyTable
+# table = PrettyTable(['Pokemon Name', 'Type'])
+# table.add_row(['Pikachu', 'Electric'])
+# table.add_row(['Squirtle', 'Water'])
+# table.add_row(['Charmander', 'Fire'])
+# table = PrettyTable()
+# table.add_column('Pokemon Name', ['Pikachu', 'Squirtle', 'Charmander'])
+# table.add_column('Type', ['Electric', 'Water', 'Fire'])
+# table.align = 'l'
+# print(table)
